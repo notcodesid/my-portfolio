911da52ab40e48aea94d489f619f4ba0ebc3cdc9
siddharth <notcodesid@gmail.com>
2025-01-02 12:02:22 -0500

    small later

--- components/projects.tsx
+++ components/projects.tsx
@@ -2,39 +2,39 @@ import ProjectCard from "./ProjectCard";
 
 const projects = [
   {
-      title: "Lolify ( Frontend )",
-      description: "Lolify creates branded memes to boost engagement and drive better campaign results.",
+      title: "lolify ( frontend )",
+      description: "lolify creates branded memes to boost engagement and drive better campaign results.",
       image: "/projects/project-5.png",
       liveSite: "https://lolify.io/",
       techStack: ["Next" ,"React", "Tailwind"],
     },
     {
-    title: "Blogging Website",
-    description: "A blogging site for exploring code, sharing knowledge, and building the future.",
+    title: "blogging website",
+    description: "a blogging site for exploring code, sharing knowledge, and building the future.",
     image: "/projects/project-1.png",
     liveSite: "https://sidblogs.vercel.app/",
     github: "https://github.com/notcodesid/personal-blogging",
     techStack: ["Next", "Tailwind", "TypeScript" , "Cloudflare", "Postgres"],
   },
   {
-    title: "Ask Me Anything",
-    description: "For getting all the doubts and question at one place",
+    title: "ask me anything",
+    description: "for getting all the doubts and question at one place",
     image: "/projects/project-2.png",
     liveSite: "https://ama-beige.vercel.app/",
     github: "https://github.com/notcodesid/askmeanything",
     techStack: ["Next", "TypeScript", "Tailwind", "Mongo"],
   },
   {
-    title: "Learn Web Dev",
-    description: "A basic roadmap and resources for learning Web development.",
+    title: "learn web dev",
+    description: "a basic roadmap and resources for learning Web development.",
     image: "/projects/project-3.png",
     liveSite: "https://learnwebdev.vercel.app/",
     github: "https://github.com/notcodesid/learnwebdev",
     techStack: ["React", "Next", "Tailwind"],
   },
   {
-    title: "Dognosis",
-    description: "Reimagining the relationship between humans and our best friends",
+    title: "dognosis",
+    description: "reimagining the relationship between humans and our best friends",
     image: "/projects/project-4.png",
     liveSite: "https://dognosis.vercel.app/",
     github: "https://github.com/notcodesid/dognosis",
@@ -42,8 +42,8 @@ const projects = [
   },
  
   {
-    title: "Typing Tester",
-    description: "A typing tester that lets you check your typing speed.",
+    title: "typing tester",
+    description: "a typing tester that lets you check your typing speed.",
     image: "/projects/project-6.png",
     liveSite: "https://typing-tester-phi.vercel.app/",
     github: "https://github.com/notcodesid/typing-tester",
@@ -56,10 +56,9 @@ export default function Projects() {
     <section className="w-full py-12 md:py-24 lg:py-32" id="projects">
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
-          <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">My Projects</h2>
+          <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">my projects</h2>
           <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-            Check out some of the projects I have worked on. I have built a diverse
-            portfolio showcasing my skills and creativity.
+            check out some of the projects i have worked on.
           </p>
         </div>
         <div className="mt-12 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
