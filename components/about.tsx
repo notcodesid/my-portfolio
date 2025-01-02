cf215b116edf846a8f4dc540ccfd2dfb0f4ff8a5
siddharth <notcodesid@gmail.com>
2025-01-02 11:52:03 -0500

    small later

--- components/about.tsx
+++ components/about.tsx
@@ -4,26 +4,26 @@ export default function About() {
       <section className="w-full py-12 md:py-24 lg:py-32 bg-muted">
         <div className="container grid items-center justify-center gap-8 px-4 md:px-6 lg:grid-cols-2 lg:gap-16">
           <div className="space-y-4">
-            <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">About Me</h2>
+            <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">about me</h2>
             <p className="max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-            Hey! I&apos;m Siddharth. I&apos;m really into making websites. Been doing it for about a year now. I work with stuff like <span className="font-bold text-red-500 "> TypeScipt, JavaScript, React, and Node.js.</span>  
+            hey! I&apos;m siddharth. i&apos;m really into making websites. been doing it for about a year now. i work with stuff like <span className="font-bold text-red-500 "> typescipt, javascript, react, and node.js.</span>  
             
             <br />
             <br />
-            Love making sites that look good and work great!
+            love making sites that look good and work great.
             </p>
           </div>
           <div className="grid gap-4">
             <div className="grid gap-1">
-              <h3 className="text-xl font-bold">Background</h3>
+              <h3 className="text-xl font-bold">background</h3>
               <p className="text-muted-foreground">
-              I&apos;m pursuing a Bachelor&apos;s degree in Science., by the way. I&apos;ve worked with small teams and done some Hackathons too. Always trying to learn new tech stuff - I&apos;m kinda addicted to keeping up with what&apos;s new in the industry!
+              i&apos;m pursuing a bachelor&apos;s degree in science., by the way. I&apos;ve worked with small teams and done some Hackathons too. always trying to learn new tech stuff - i&apos;m kinda addicted to keeping up with what&apos;s new in the industry!
               </p>
             </div>
             <div className="grid gap-1">
-              <h3 className="text-xl font-bold">Skills</h3>
+              <h3 className="text-xl font-bold">skills</h3>
               <p className="text-muted-foreground">
-              My main skills? I&apos;m really good with TypeScript, JavaScript, React, and Node.js. I also know my way around NextJS, ExpressJS, and a bunch of other cool libraries. I can make websites look great on any device, and I get the whole UI/UX thing. Oh, and I&apos;m pretty handy with Git for keeping track of all my code changes.
+              my main skills? i&apos;m really good with typescript, javascript, react, and node.js. i also know my way around nextjs, expressjs, and a bunch of other cool libraries. i can make websites look great on any device, and I get the whole ui/ux thing. oh, and i&apos;m pretty handy with git for keeping track of all my code changes.
               </p>
             </div>
     
--- components/heroSection.tsx
+++ components/heroSection.tsx
@@ -7,12 +7,12 @@ export function HeroSection() {
   return (
     <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
       <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-7xl font-bold leading-none tracking-tighter text-transparent">
-        Siddharth Pandro
+        siddharth pandro
           </span>
          <div className="max-w-2xl mx-auto space-y-6">
-          <h1 className="text-4xl md:text-6xl font-bold text-center tracking-tight">Mastering the Full Stack</h1>
+          <h1 className="text-4xl md:text-6xl font-bold text-center tracking-tight mt-10">full-stack developer.</h1>
           <p className="text-lg md:text-xl text-center">
-            Crafting seamless experiences with expertise in both frontend and backend development.
+            crafting seamless experiences with expertise in both frontend and backend development.
           </p>
         </div>
 
