3a6e5f335e074570a193c6fe97abf22d0314af62
notcodesid <notcodesid@gmail.com>
2025-04-02 13:25:19 +0530

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -171,7 +171,7 @@ export default function Home() {
                 <header className="lowercase text-sm">
                   <h3 className="text-neutral-8 dark:text-neutral-dark-8">
                     <Link href="https://github.com/notcodesid/Accountability-App" className="hover:bg-neutral-2 active:bg-neutral-3 dark:hover:bg-neutral-dark-2 dark:active:bg-neutral-dark-3 focus:outline-none focus-visible:ring-2 focus-visible:ring-neutral-7 dark:focus-visible:ring-neutral-dark-7 px-2 -mx-2 py-1 -my-1 rounded-lg">
-                    accountablity app
+                    accountability app
                       <span className="not-sr-only"> ↗</span>
                     </Link>
                   </h3>
